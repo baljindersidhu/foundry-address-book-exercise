db04b365a5acc05ea12c18cb655501a0ba0e129c
baljindersidhu <baljinders51@gmail.com>
2021-04-28 22:22:39 +0530

    Feature: Added a preload file

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const { join } = require("path");
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+          preload: join(__dirname, "preload.js"),
+        }
     });
     
     win.loadFile(join(__dirname, `/dist/index.html`));
